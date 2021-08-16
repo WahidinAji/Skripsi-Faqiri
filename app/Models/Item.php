2caa0a1ff31f811a99e39ea36e9d7c4357518791
WahidinAji <a17wahidin@gmail.com>
2021-08-16 19:38:09 +0700

    add softdelete

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -3,11 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $guarded = ['id'];
     public $timestamps = true;
 
