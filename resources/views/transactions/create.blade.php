c7a798f38a444f6e77998fe51a39cf4625a94320
WahidinAji <a17wahidin@gmail.com>
2021-08-24 12:54:02 +0700

    select autofill done. but not ready to use

--- resources/views/transactions/create.blade.php
+++ resources/views/transactions/create.blade.php
@@ -5,27 +5,9 @@
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 @section('main-content')
 <div class="container">
-    <div class="m-3 p-3">
-        <select class="js-example-data-ajax form-control">
-            <option value="3620194" selected="selected">select2/select2</option>
-        </select>
-        <input type="text" class="test">
-        <input type="text" class="test2">
-        <input type="text" class="test3">
-    </div>
-    <div class="m-3 p-3">
-        <select class="js-example-data-ajax form-control">
-            <option value="3620194" selected="selected">select2/select2</option>
-        </select>
-        <input type="text" class="test">
-        <input type="text" class="test2">
-        <input type="text" class="test3">
-    </div>
-    <div class="m-3 p-3">
-        <select class="js-example-data-ajax2 form-control">
-            {{-- <option value="3620194" selected="selected">2</option> --}}
-        </select>
-    </div>
+    <form action="{{ route('transactions.store') }}" id="form2">
+        @csrf
+    </form>
 </div>
 
 <form method="POST" action="{{ route('transactions.store') }}" id="form">
@@ -92,6 +74,99 @@
 @endpush
 @push('script')
 <script>
+    $(document).ready(function () {
+        var count = 1;
+        dynamic_field(count);
+        function dynamic_field(number) {
+            html = '<div class="form-group row" id="row_form">';
+            html += `<label for="item_id" class="col-sm-2 col-form-label pr-0">Nama Barang </label>`;
+            html += `
+            <div class="col-sm-4">
+                <select class="js-example-data-ajax form-control" id="name" name="name[]">
+                </select>
+            </div>
+            <div class="col-sm">
+                <input type="text" id="desc" class="form-control form-control-sm" name="desc[]">
+            </div>
+            <div class="col-sm">
+                <input type="text" id="link" class="form-control form-control-sm" name="link[]">
+            </div>`;
+            if (number > 1) {
+                html += `
+                <div class="col-sm">
+                    <button type="button" name="remove" id="remove" class="btn btn-danger">Remove</button>
+                </div>
+                `;
+                $('#form2').append(html);
+            } else {
+                html += `
+                <div class="col-sm">
+                    <button type="button" name="adding" id="add" class="btn btn-success">Add</button></td>
+                </div>
+                `;
+                $('#form2').html(html);
+            }
+        }
+        $(document).on('click', '#add', function () {
+            count++;
+            dynamic_field(count);
+        });
+        $(document).on('click', '#remove', function () {
+            count--;
+            $(this).closest("#row_form").remove();
+        });
+        $(".js-example-data-ajax").select2({
+            ajax: {
+                url: "https://api.github.com/search/repositories",
+                dataType: 'json',
+                delay: 250,
+                data: function (params) {
+                    return {
+                        q: params.term, // search term
+                        page: params.page
+                    };
+                },
+                processResults: function (data, params) {
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+
+                    return {
+                        results: data.items,
+                        pagination: {
+                            more: (params.page * 30) < data.total_count
+                        }
+                    };
+                },
+                cache: true
+            },
+            placeholder: 'Search for a repository',
+            minimumInputLength: 1,
+            templateResult: formatRepo,
+            templateSelection: formatRepoSelection
+        });
+
+        function formatRepo(repo) {
+            if (repo.loading) {
+                return repo.text;
+            }
+            var $container = $(
+                '<p>'+repo.full_name+'</p>'
+            );
+            return $container;
+        }
+
+        function formatRepoSelection(repo) {
+            $(`#name`).val(repo.full_name);
+            $(`#desc`).val(repo.description);
+            $(`#link`).val(repo.id);
+            return repo.full_name || repo.text;
+        }
+    });
+</script>
+<script>
     function formatState (state) {
     if (!state.id) {
         return state.text;
@@ -111,7 +186,7 @@
         templateSelection: formatState
     });
 </script>
-<script>
+{{-- <script>
     $(".js-example-data-ajax").select2({
         ajax: {
             url: "https://api.github.com/search/repositories",
@@ -185,7 +260,7 @@
         return repo.full_name || repo.text;
     }
 
-</script>
+</script> --}}
 <script>
     $(function () {
         var projects = "{{ route('items.search') }}";
