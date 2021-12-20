6c9ef7e23c1ce4777b8287718b47f06c06c8ce01
WahidinAji <a17wahidin@gmail.com>
2021-12-20 16:44:53 +0700

    update query

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -22,7 +22,7 @@ class CartController extends Controller
             ->select(DB::raw('count(id) as id, category'))
             ->groupBy('category')
             ->get();
-        $items = Item::where('category', 'herbivora')->select('id', 'code', 'name', 'price', 'type', 'stock')->get();
+        $items = Item::select('id', 'code', 'name', 'price', 'type', 'stock')->limit(100)->get();
         if (\request()->has('category')) {
             $category = \request('category');
             $items = Item::where('category', $category)->select('id', 'code', 'name', 'price', 'type', 'stock')->get();
